7a4081115e10bd921d78b5caea242a4e884bf60e
Dana Kiehl <dana.palm87@gmail.com>
2020-04-24 08:45:54 -0500

    Init, created a FunWithLoopsClass and created a for loop that will run 5 times

---
+++ CriticalThinking/FunWithForLoops.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriticalThinking
+{
+    class FunWithForLoops
+    {
+        //member variables
+
+        //constructor
+        public FunWithForLoops()
+        {
+
+        }
+
+        //member methods
+        public void ForLoop()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+
+            }
+        }
+    }
+}

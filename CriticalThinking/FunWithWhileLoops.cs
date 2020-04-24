b7febffc34d8756754f8b99e275c8bb533319115
Dana Kiehl <dana.palm87@gmail.com>
2020-04-24 10:19:48 -0500

    created a FunWithWhileLoops class and a while loop method with a while loop inside.

---
+++ CriticalThinking/FunWithWhileLoops.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriticalThinking
+{
+    class FunWithWhileLoops
+    {
+        
+        //member variables
+
+        //constructer
+
+        //member methods
+        public void WhileLoop()
+        {
+            int counter = 5;
+            while(counter < 25)
+            {
+                counter++;
+            }
+
+        }
+        
+        
+
+
+    }
+}
